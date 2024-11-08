4baf07bfd0a22d697bab0cdd7d3a5bd3ed7dbf78
Jenrikku <jenrikku@users.noreply.github.com>
2024-11-08 10:35:27 +0100

    Add btm reading in tilemaptool

--- tools/tilemaptool/Context/ContextHandler.cs
+++ tools/tilemaptool/Context/ContextHandler.cs
@@ -1,11 +1,24 @@
-using TilemapTool.Storage;
+using LibBTM;
 
 namespace TilemapTool.Context;
 
 internal static class ContextHandler
 {
-    public static Tile[,] Map { get; } = new Tile[10, 10];
-    public static List<Tile> TileSet { get; } = new();
+    public static BTM? BTM { get; private set; }
 
-    public static bool IsSaved { get; } = true; // TO-DO
+    public static bool IsSaved { get; set; } = true;
+
+    /// <summary>
+    /// Reads a BTM and sets it as the current opened one.
+    /// </summary>
+    /// <param name="path">The path to the BTM</param>
+    /// <returns>Whether it was read correctly</returns>
+    public static bool ReadBTM(string path)
+    {
+        if (!BTM.Identify(path))
+            return false;
+
+        BTM = BTM.Read(path);
+        return true;
+    }
 }
--- tools/tilemaptool/Storage/Tile.cs
+++
@@ -1,6 +0,0 @@
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
-
-namespace TilemapTool.Storage;
-
-internal class Tile { }
